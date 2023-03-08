07ce28943887ba53856da187521f3516aed6f2d7
jhossepmv <jhossepmv@gmail.com>
2023-03-08 16:48:49 -0300

    "added 271"

--- Blind 75/Arrays & Hashing/238.product_of_array_except_self.py
+++ Blind 75/Arrays & Hashing/238.product_of_array_except_self.py
@@ -1,11 +1,18 @@
 def productExceptSelf(nums):
-    for i in range(len(nums)):
-        for n in nums:
-            if n == nums[i]:
-                continue
-            elif 
+    res = [1] * (len(nums))
 
+    prefix = 1
+    for i in range(len(nums)):
+        res[i] = prefix
+        prefix *= nums[i]
+
+    postfix = 1
+    for i in range(len(nums) - 1, -1, -1):
+        res[i] *= postfix
+        postfix *= nums[i]
+    return res
 
 nums = [1,2,3,4]
+# nums = [-1,1,0,-3,3]
 
 print(productExceptSelf(nums))
